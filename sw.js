7592fca6c9bdad9083b02021174c03bc0c87dcda
lucassmartibs-dot <lucas.smartibs@gmail.com>
2026-08-13 15:41:42 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+const C='central-lucas-v1';
+
+self.addEventListener('install',e=>{
+  e.waitUntil(
+    caches.open(C)
+      .then(c=>c.addAll(['./','./index.html','./manifest.webmanifest']))
+      .then(()=>self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate',e=>{
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch',e=>{
+  if(e.request.method==='GET'){
+    e.respondWith(
+      caches.match(e.request).then(r=>r||fetch(e.request))
+    );
+  }
+});
