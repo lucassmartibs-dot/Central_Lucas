ce75bf8e8152f084081c699b7df737b056227e3b
lucassmartibs-dot <lucas.smartibs@gmail.com>
2026-09-15 13:31:16 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,43 @@
-const C='central-lucas-v1';
+const C='central-lucas-v2';
+const ASSETS=['./','./index.html','./manifest.webmanifest'];
 
 self.addEventListener('install',e=>{
   e.waitUntil(
     caches.open(C)
-      .then(c=>c.addAll(['./','./index.html','./manifest.webmanifest']))
+      .then(c=>c.addAll(ASSETS))
       .then(()=>self.skipWaiting())
   );
 });
 
 self.addEventListener('activate',e=>{
-  e.waitUntil(self.clients.claim());
+  e.waitUntil(
+    caches.keys()
+      .then(keys =>
+        Promise.all(
+          keys
+            .filter(k=>k!==C)
+            .map(k=>caches.delete(k))
+        )
+      )
+      .then(()=>self.clients.claim())
+  );
 });
 
 self.addEventListener('fetch',e=>{
-  if(e.request.method==='GET'){
-    e.respondWith(
-      caches.match(e.request).then(r=>r||fetch(e.request))
-    );
-  }
+  if(e.request.method!=='GET') return;
+
+  const url=new URL(e.request.url);
+  if(url.origin!==self.location.origin) return;
+
+  e.respondWith(
+    fetch(e.request)
+      .then(response=>{
+        const copy=response.clone();
+        caches.open(C).then(cache=>{
+          cache.put(e.request,copy);
+        });
+        return response;
+      })
+      .catch(()=>caches.match(e.request))
+  );
 });
