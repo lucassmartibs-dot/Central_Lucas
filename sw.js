1ac4d957ea79ffab16a49ab82d60d3838f31b0dd
lucassmartibs-dot <lucas.smartibs@gmail.com>
2026-09-15 13:50:46 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -12,30 +12,22 @@ self.addEventListener('install',e=>{
 self.addEventListener('activate',e=>{
   e.waitUntil(
     caches.keys()
-      .then(keys =>
-        Promise.all(
-          keys
-            .filter(k=>k!==C)
-            .map(k=>caches.delete(k))
-        )
-      )
+      .then(keys=>Promise.all(
+        keys.filter(k=>k!==C).map(k=>caches.delete(k))
+      ))
       .then(()=>self.clients.claim())
   );
 });
 
 self.addEventListener('fetch',e=>{
   if(e.request.method!=='GET') return;
-
-  const url=new URL(e.request.url);
-  if(url.origin!==self.location.origin) return;
+  if(new URL(e.request.url).origin!==self.location.origin) return;
 
   e.respondWith(
     fetch(e.request)
       .then(response=>{
         const copy=response.clone();
-        caches.open(C).then(cache=>{
-          cache.put(e.request,copy);
-        });
+        caches.open(C).then(cache=>cache.put(e.request,copy));
         return response;
       })
       .catch(()=>caches.match(e.request))
